4e827c3cf2913d4123f0e8c87188c7814eb46198
Divnag <divyamn.shine@gmail.com>
2018-02-02 17:00:46 -0500

    Intial commit

---
+++ VirtualPet/DigiPet.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class DigiPet
+    {
+        private string name;
+        private int hunger;
+        private int thirst;
+        private string mood;
+        private int boredom;
+        private string dance;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.Name = value; }
+        }
+
+
+        public int Hunger
+        {
+            get { return this.hunger; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+        }
+        public string Mood
+        {
+            get { return this.mood; }
+        }
+
+
+        public int Boredom
+        {
+            get { return this.boredom; }
+        }
+
+        public string Dance
+        {
+            get { return this.dance; }
+        }
+
+
+
+        public DigiPet()
+        {
+            
+            this.hunger = 10;
+            this.thirst = 0;
+            this.boredom = 5;
+            this.mood = "Happy";
+            
+        }
+        
+          public DigiPet(string name)
+        {
+
+            this.name = name;
+
+        }
+
+
+
+        public void Feed()
+        {
+            Console.WriteLine("Done Feeding\n");
+            hunger-=10;
+            thirst++;
+           
+                   
+        }
+
+        public void Thirsty()
+        {
+            Console.WriteLine("Drank Water\n");
+            thirst--;
+            
+        }
+
+        public void Cuddle()
+        {
+            Console.WriteLine("Time to cuddle\n");
+            mood = "Happy";
+            boredom-=5;
+        }
+        public void MusicTime()
+        {
+            
+              Console.WriteLine("Do some good Moves with me \a \a\n");
+            mood = "Happy";
+            boredom -= 5;
+
+            hunger +=5;
+        }
+
+        public void Tick()
+        {
+            Console.WriteLine("Too Tired of doing activities\n");
+            hunger+=10;
+            Console.WriteLine("Time to eat\n");
+
+            
+            if (hunger >= 5)
+            {
+                boredom+=5;
+               
+            }
+            if (boredom == 5)
+            {
+                mood = "sad";
+                Console.WriteLine("Feeling sad\n");
+                dance = "yes";
+            }
+
+            Console.WriteLine("Current status of your Pet");
+            Console.WriteLine("Hunger------ " + hunger);
+            Console.WriteLine("Thirst level ---------" + thirst);
+            Console.WriteLine("Mood of the Dog-------- " + mood);
+            Console.WriteLine("Bored------- " + boredom);
+            Console.WriteLine("Want to Dance\n");
+        }
+            
+            
+    }
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("What do you want to name your Digibird");
+            string name = Console.ReadLine();
+            DigiPet petOne = new DigiPet(name);
+            Console.WriteLine("Your Pet name :" + name);
+            petOne = new DigiPet();
+
+            int userOption;
+            Console.WriteLine("\nCurrent status of your Pet");
+            Console.WriteLine("Hunger------ " + petOne.Hunger);
+            Console.WriteLine("Thirst level------" + petOne.Thirst);
+            Console.WriteLine("Mood of the Bird-----" + petOne.Mood);
+            Console.WriteLine("Want to go to Dance  \n");
+            Console.WriteLine("What do you want to do with your pet");
+            int count = 0;
+            do
+            {
+
+                Console.WriteLine("To Feed Food: select: 1");
+                Console.WriteLine("To Feed water: select: 2");
+                Console.WriteLine("To Cuddle: select: 3");
+                Console.WriteLine("To Danace: select: 4");
+                Console.WriteLine("To see status with Pet select: 5");
+
+
+                userOption = int.Parse(Console.ReadLine());
+
+                switch (userOption)
+                {
+                    case 1:
+                        petOne.Feed();
+                        count ++;
+                        Console.WriteLine(count);
+                        break;
+                    case 2:
+
+                        petOne.Thirsty();
+                        count += 1;
+                        break;
+                    case 3:
+
+                        petOne.Cuddle();
+                        count += 1;
+                        break;
+                    case 4:
+
+                        petOne.MusicTime();
+                        count += 1;
+                        break;
+                    case 5:
+                        Console.WriteLine("Current status of your Pet");
+                        Console.WriteLine("Hunger------ " + petOne.Hunger);
+                        Console.WriteLine("Thirst level ---------" + petOne.Thirst);
+                        Console.WriteLine("Mood of the Dog-------- " + petOne.Mood);
+                        Console.WriteLine("Bored------- " + petOne.Boredom);
+                        Console.WriteLine("Want to Dance\n");
+
+                        break;
+                    default:
+
+                        Console.WriteLine("Choose appropriate action");
+                        break;
+                  
+                }
+                if (count >= 3)
+                {
+                    petOne.Tick();
+                }
+                //Console.WriteLine("Current status of your Pet");
+                //Console.WriteLine("Hunger------ " + petOne.Hunger);
+                //Console.WriteLine("Thirst level ---------" + petOne.Thirst);
+                //Console.WriteLine("Mood of the Dog-------- " + petOne.Mood);
+                //Console.WriteLine("Bored------- " + petOne.Boredom);
+                //Console.WriteLine("Want to Dance\n");
+                
+            } while(userOption!=6);
+            
+        }
+        
+     }
+}
+
+
+
